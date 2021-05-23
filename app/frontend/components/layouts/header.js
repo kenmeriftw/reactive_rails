0ff5886a346ae3c07c949cd3059137bfdd755ec3
Boris Parusnikov <2669997@gmail.com>
2021-05-23 21:02:54 +0300

    translations added

---
+++ app/frontend/components/layouts/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+
+const Header = () => {
+  const { t, i18n } = useTranslation('common').t
+
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Toggle aria-controls = 'basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href="#">{t('navi.contacts')}</Nav.Link>
+          <Nav.Link href="#">{t('navi.add_contact')}</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+
+    </Navbar>
+  )
+}
+
+export default Header
