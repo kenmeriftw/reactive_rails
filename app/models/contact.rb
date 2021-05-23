a0f7e3180ec98215090a2dbafa6e7db036e2fe35
Boris Parusnikov <2669997@gmail.com>
2021-05-23 20:30:17 +0300

    Add contacts API

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   scope :sorted, -> { order(:name) }
 
-  vaildates :name, :email, presence: true
-  validates :email, format: { URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :name, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :contacts, only: %i[create index]
+    end
+  end
+
   root 'index#index'
 end
